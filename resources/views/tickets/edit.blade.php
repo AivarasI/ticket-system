db0d452b9426760da3978644b9434ea085aab0ed
AivarasI <aivaras.andrijaitis@stud.svako.lt>
2026-01-04 17:09:09 +0200

    ištrintas sudubliuotas status mygtukas

--- resources/views/tickets/edit.blade.php
+++ resources/views/tickets/edit.blade.php
@@ -28,7 +28,7 @@
                     @endforeach
                 </select>
             </div>
-
+ 
             <div class="mt-4">
                 <label>Status</label>
                 <select name="status" required class="border w-full">
@@ -75,18 +75,6 @@
     </div>
 
 
-    @if(auth()->user()->isAdmin())
-    <div class="mt-4">
-        <label>Status</label>
-        <select name="status" class="border w-full">
-            @foreach($statuses as $status)
-                <option value="{{ $status }}"
-                    @selected($ticket->status === $status)>
-                    {{ ucfirst(str_replace('_', ' ', $status)) }}
-                </option>
-            @endforeach
-        </select>
-    </div>
-@endif
+
    
 </x-app-layout>
